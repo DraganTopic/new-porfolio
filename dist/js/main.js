0a3dce3f60866b997b4c84be48d42ee70da81bde
DraganTopic <dragantopic32@gmail.com>
2018-11-05 01:18:46 +0100

    Menu overlay

--- dist/js/main.js
+++ dist/js/main.js
@@ -15,8 +15,8 @@ function toggleMenu() {
   if (!showMenu) {
     menuBtn.classList.add("close");
     menu.classList.add("show");
-    menuBtn.classList.add("show");
-    menuBtn.classList.add("show");
+    menuNav.classList.add("show");
+    menuBranding.classList.add("show");
     navItems.forEach(item => item.classList.add("show"));
 
     // Set Menu State
@@ -24,8 +24,8 @@ function toggleMenu() {
   } else {
     menuBtn.classList.remove("close");
     menu.classList.remove("show");
-    menuBtn.classList.remove("show");
-    menuBtn.classList.remove("show");
+    menuNav.classList.remove("show");
+    menuBranding.classList.remove("show");
     navItems.forEach(item => item.classList.remove("show"));
 
     // Set Menu State
